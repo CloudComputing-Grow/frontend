01022ab99b8222f761e8d11d9c741e4c596d3cb0
thatsgoodd <dltndus5985@gmail.com>
2026-06-01 10:49:53 +0900

    feat: vite.config.js 서비스별 프록시 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3002'
+      '/auth': 'http://localhost:3001',
+      '/user': 'http://localhost:3000',
+      '/achievement': 'http://localhost:3002',
+      '/api/v1/missions': 'http://localhost:3003',
+      '/api/v1/admin': 'http://localhost:3003',
+      '/api/v1/inventory': 'http://localhost:3004',
+      '/api/v1/market': 'http://localhost:3004',
+      '/api/v1/growth-diary': 'http://localhost:3005',
+      '/api/v1/community': 'http://localhost:3006',
     }
   }
 })
